f259ce493a86b567f308fcb2cd9c7dd3606a383f
kanunnikov-developer <kolapsinfo@mail.ru>
2026-03-31 19:17:22 +0300

    Добавил Самару

--- src/lib/cities.ts
+++ src/lib/cities.ts
@@ -3,23 +3,28 @@ const site = 'girakequuce.beget.app';
 
 export const cities = [
 	{
-		id: 1,
+		id: 0,
 		name: 'Москва',
 		href: `${link}msc.${site}`,
 	},
 	{
-		id: 2,
+		id: 1,
 		name: 'Ростов',
 		href: `${link}rostov.${site}`,
 	},
 	{
-		id: 3,
+		id: 2,
 		name: 'Краснодар',
 		href: `${link}krasnodar.${site}`,
 	},
 	{
-		id: 4,
+		id: 3,
 		name: 'Санкт-Петербург',
 		href: `${link}spb.${site}`,
+	},
+	{
+		id: 4,
+		name: 'Самара',
+		href: `${link}samara.${site}`,
 	},
 ];
