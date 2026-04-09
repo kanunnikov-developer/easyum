c9d1590b28f126a18d6387e7a2240c5f981affaa
kanunnikov-developer <kolapsinfo@mail.ru>
2026-04-10 00:43:40 +0300

    Добавил табы

--- src/app/blog/model.ts
+++ src/app/blog/model.ts
@@ -5,6 +5,7 @@ export type Post = {
 	date: string;
 	description: string;
 	href: string;
+	category: string;
 }
 
 export const posts: Post[] = [
@@ -15,6 +16,7 @@ export const posts: Post[] = [
 		date: '8 апреля 2026',
 		description: 'Front-end разработчик – программист, который создает внешнюю часть сайта или веб-приложения. Он разрабатывает то, с чем вы взаимодействуете, когда выбираете товары в интернет-магазине или ...',
 		href: '/blog/kto-takoj-front-end-razrabotchik',
+		category: 'Программирование',
 	},
 	{
 		id: 2,
@@ -23,6 +25,7 @@ export const posts: Post[] = [
 		date: '9 апреля 2026',
 		description: 'Кто такой тестировщик ПО, чем он занимается, нужен ли код, как выглядит работа тестировщиком и почему это один из самых понятных способов войти в IT в 2026 году. Пошаговый план, плюсы и минусы, FAQ и мини-тест.',
 		href: '/blog/testing-po-v-2026-godu',
+		category: 'Тестирование',
 	},
 	{
 		id: 3,
@@ -31,6 +34,7 @@ export const posts: Post[] = [
 		date: '9 апреля 2026',
 		description: 'Какой язык программирования учить первым в 2026 году: рейтинг для новичков. Пошаговый план, плюсы и минусы, FAQ и мини-тест.',
 		href: '/blog/kakoj-yazyk-programmirovaniya-uchit-pervym',
+		category: 'Программирование',
 	},
 	{
 		id: 4,
@@ -39,5 +43,6 @@ export const posts: Post[] = [
 		date: '9 апреля 2026',
 		description: 'Мы часто слышим опасения: «IT — это сложно», «нужно быть программистом», «я слишком стар». Эти страхи понятны, но в 2026 году они абсолютно беспочвенны. Современная IT-индустрия — это не только код',
 		href: '/blog/kak-voity-v-it-s-nula-2026',
+		category: 'Карьера',
 	},
 ]
