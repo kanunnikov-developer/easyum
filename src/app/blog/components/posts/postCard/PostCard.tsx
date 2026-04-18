67a70784e2c1afa4429a3681cb757d0e0fcc855b
kanunnikov-developer <kolapsinfo@mail.ru>
2026-04-18 21:20:58 +0300

    Правки

--- src/app/blog/components/posts/postCard/PostCard.tsx
+++ src/app/blog/components/posts/postCard/PostCard.tsx
@@ -1,27 +1,28 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from './styles.module.css'
 import { Post } from '@/app/blog/model'
 
 export default function PostCard({ post }: { post: Post }) {
 	return(
-		<div className={styles.post}>
+		<Link href={post.href} className={styles.post}>
 			<div>
 				<div className={styles.image}>
-				<Image src={post.image} alt="Картинка поста" width={397} height={223} className={styles.img}/>
-			</div>
-			<div className={styles.content}>
-				<div className={styles.date}>
-					<img src='/blog/calendar-icon.svg' />
-					<p>{post.date}</p>
+					<Image src={post.image} alt="Картинка поста" width={397} height={223} className={styles.img}/>
 				</div>
-				<h3 className={styles.title}>{post.title}</h3>
-				<p className={styles.description}>{post.description}</p>
-			</div>
+				<div className={styles.content}>
+					<div className={styles.date}>
+						<img src='/blog/calendar-icon.svg' alt="Календарь" />
+						<p>{post.date}</p>
+					</div>
+					<h3 className={styles.title}>{post.title}</h3>
+					<p className={styles.description}>{post.description}</p>
+				</div>
 			</div>
 			
 			<div className={styles.button}>
-				<a href={post.href} className={styles.link}>Читать статью</a>
+				<span className={styles.link}>Читать статью</span>
 			</div>
-		</div>
+		</Link>
 	)
 }
